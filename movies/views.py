24f9352ed4b2e1964dabb86f9c383e9456ba2e2b
thaibcm <thaiggfotos@gmail.com>
2026-05-20 21:29:53 -0300

    feat: Itens ficado para trás

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .services import TMDBService
 
-# Create your views here.
+def movie_search(request):
+    query = request.GET.get('q','')  
+    results = []
+
+    if query:
+        tmdb_service = TMDBService()
+        results = tmdb_service.search_movies(query)
+    
+    context = {
+        'movies': results
+        'query': query
+    }
+    return render(request, 'movies/search.html', context)
+
+def movie_detail(request, movie_id):
+    tmdb_service = TMDBService()
+    movie = tmdb_service.get_movie_details(movie_id)
+
+    context = {
+        'movie': movie 
+    }    
+    return render(request, 'movies/movie_detail.html', context)
