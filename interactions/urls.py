09589fd27c540495b9f6622fe8e91b9ba2831830
R0GERr7 <toninhoroger@gmail.com>
2026-05-21 21:44:57 -0300

    up

--- interactions/urls.py
+++ interactions/urls.py
@@ -4,7 +4,17 @@ from . import views
 app_name = 'interactions'
 
 urlpatterns = [
+    # Avaliação rápida por estrelas
+    path('rate/<int:tmdb_id>/<int:rating>/', views.rate_movie, name='rate_movie'),
+    
+    # Review completa com comentário
     path('review/<int:tmdb_id>/', views.add_review, name='add_review'),
     path('review/delete/<int:review_id>/', views.delete_review, name='delete_review'),
+    
+    # Favoritos
     path('favorite/<int:tmdb_id>/', views.toggle_favorite, name='toggle_favorite'),
+    
+    # Páginas do usuário
+    path('my-reviews/', views.my_reviews, name='my_reviews'),
+    path('my-favorites/', views.my_favorites, name='my_favorites'),
 ]
--- interactions/views.py
+++ interactions/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
 from movies.models import Movie
 from .models import Review, Favorite
 from .forms import ReviewForm
 
 @login_required
+def rate_movie(request, tmdb_id, rating):
+    """Avaliação rápida por estrelas (tipo Letterboxd)"""
+    movie = get_object_or_404(Movie, tmdb_id=tmdb_id)
+    
+    # Pega review existente ou cria nova
+    review, created = Review.objects.get_or_create(
+        user=request.user,
+        movie=movie,
+        defaults={'rating': rating, 'comment': ''}
+    )
+    
+    if not created:
+        # Se já existe, atualiza a nota
+        review.rating = rating
+        review.save()
+        messages.success(request, f'Nota atualizada para {rating}★')
+    else:
+        messages.success(request, f'Você avaliou {movie.title} com {rating}★')
+    
+    return redirect('movies:detail', tmdb_id=movie.tmdb_id)
+
+@login_required
 def add_review(request, tmdb_id):
+    """Escrever avaliação completa (com comentário)"""
     movie = get_object_or_404(Movie, tmdb_id=tmdb_id)
     existing = Review.objects.filter(user=request.user, movie=movie).first()
     
@@ -17,31 +41,49 @@ def add_review(request, tmdb_id):
             review.user = request.user
             review.movie = movie
             review.save()
-            messages.success(request, 'Review salva com sucesso!')
+            messages.success(request, 'Sua avaliação foi publicada!')
             return redirect('movies:detail', tmdb_id=movie.tmdb_id)
     else:
         form = ReviewForm(instance=existing)
     
-    return render(request, 'interactions/review_form.html', {'form': form, 'movie': movie})
+    return render(request, 'interactions/review_form.html', {
+        'form': form,
+        'movie': movie,
+        'existing': existing
+    })
 
 @login_required
 def delete_review(request, review_id):
+    """Deletar avaliação"""
     review = get_object_or_404(Review, id=review_id, user=request.user)
-    tmdb_id = review.movie.tmdb_id
+    movie_id = review.movie.tmdb_id
     review.delete()
-    messages.success(request, 'Review removida!')
-    return redirect('movies:detail', tmdb_id=tmdb_id)
+    messages.success(request, 'Avaliação removida.')
+    return redirect('movies:detail', tmdb_id=movie_id)
 
 @login_required
 def toggle_favorite(request, tmdb_id):
+    """Favoritar/desfavoritar filme"""
     movie = get_object_or_404(Movie, tmdb_id=tmdb_id)
     fav = Favorite.objects.filter(user=request.user, movie=movie).first()
     
     if fav:
         fav.delete()
-        messages.info(request, f'"{movie.title}" removido dos favoritos')
+        messages.info(request, f'Removido dos favoritos: {movie.title}')
     else:
         Favorite.objects.create(user=request.user, movie=movie)
-        messages.success(request, f'"{movie.title}" adicionado aos favoritos')
+        messages.success(request, f'Adicionado aos favoritos: {movie.title}')
     
     return redirect('movies:detail', tmdb_id=movie.tmdb_id)
+
+@login_required
+def my_reviews(request):
+    """Minhas avaliações (página estilo Letterboxd)"""
+    reviews = Review.objects.filter(user=request.user).select_related('movie').order_by('-created_at')
+    return render(request, 'interactions/my_reviews.html', {'reviews': reviews})
+
+@login_required
+def my_favorites(request):
+    """Meus favoritos (página estilo Letterboxd)"""
+    favorites = Favorite.objects.filter(user=request.user).select_related('movie').order_by('-created_at')
+    return render(request, 'interactions/my_favorites.html', {'favorites': favorites})
